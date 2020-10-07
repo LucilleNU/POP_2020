eeede2d5f2b26aa26d085c019d200e507159a28b
Lucille Nawa <lucillenawa@Lucilles-MacBook-Pro.local>
2020-10-07 23:34:02 +0300

    Homework

--- Lecture_1/Program.cs
+++ Lecture_1/Program.cs
@@ -1,12 +1,126 @@
 ﻿using System;
 
-namespace Lecture_1
-{
+namespace Lecture_1 {
+
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            MeAfter10();
+            SimpleFormulas();
+            Task3_Dates();
+            Task4_FutureBDay();
+
+
         }
+     private static void MeAfter10()
+        {
+            Console.Write(" please enter your age: ");
+             string ageAsString = Console.ReadLine();
+
+            try
+            {
+
+                int ageAsint = int.Parse(ageAsString);
+                int ageAfter10 = ageAsint + 10;
+
+                Console.WriteLine("Your age after 10 years will be (ageAfter10) years");
+            }
+            catch
+            {
+                Console.WriteLine("ERROR. Please enter correct age");
+            }
+        }
+        private static void SimpleFormulas ()
+        {
+            Console.WriteLine("Rectangle");
+            Console.WriteLine("---------");
+
+            Console.Write(" please enter side A: ");
+            int sideA = int.Parse(Console.ReadLine();
+
+
+            Console.Write(" please enter side B: ");
+            int sideB = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Area = " +sideA * sideB);
+
+            Console.WriteLine("Perimeter = " + 2 * (sideA + sideB);
+
+            Console.ReadKey();
+            Console.Clear();
+
+
+            Console.WriteLine("Triangle");
+            Console.WriteLine("---------");
+
+            Console.Write(" please enter side A: ");
+            int sideA = int.Parse(Console.ReadLine();
+
+
+            Console.Write(" please enter side B: ");
+            int sideB = int.Parse(Console.ReadLine());
+
+            Console.Write(" please enter side B: ");
+            int sideC = int.Parse(Console.ReadLine());
+
+            Console.Write(" please enter height: ");
+            int height = int.Parse(Console.ReadLine();
+
+
+            Console.WriteLine("Area = " + sideA * height);
+
+            Console.WriteLine("Perimeter =" + (sideA + sideB + sideC);
+
+            Console.ReadKey();
+
+        }
+        private static void Task3_Dates()
+        {
+            DateTime date = DateTime.Now;
+
+            Console.Write(" Current date: ");
+            Console.WriteLine(date.ToShortDateString());
+
+            Console.Write(" Current time: ");
+            Console.WriteLine(date.ToShortDateString());
+
+            Console.Write(" DAte after 1rs0 yea: ");
+            Console.WriteLine(date.Addyears(10).ToShortDateString());
+
+            Console.Write("PLease enter a period: ");
+            int period = int.Parse(Console.ReadLine());
+
+            Console.Write($"Date after (period) years: ");
+        }
+
+        private static void Task4_FutureBDay()
+        {
+            Console.Write(" Current age: ");
+            int currentAge = int.Parse(Console.ReadLine();
+
+
+            Console.Write(" Future age: ");
+            int futureAge = int.Parse(Console.ReadLine());
+
+            Console.Write(" BDay month: ");
+            int bdayMonth = int.Parse(Console.ReadLine());
+
+            Console.Write(" BDay day: ");
+            int bdayDay = int.Parse(Console.ReadLine();
+
+            int ageDifference = futureAge - currentAge;
+            int futureYear = DateTime.Now.Year + ageDifference;
+
+            var futureBDay = new DateTime(futureYear, bdayMonth, bdayDay);
+            Console.Write(" ");
+
+            Console.ReadKey();
+
+
+        }
+
+
+
     }
 }
---
+++ Lecture_2/Program.cs
@@ -0,0 +1,111 @@
+﻿using System;
+
+namespace Lecture_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            HW1_quest1();
+            HW1_quest2();
+            HW1_quest3();
+            HW2_quest4();
+            HW2_quest5();
+            HW2_quest6();
+            HW2_quest7();
+        }
+
+
+        private static void HW1_quest1()
+        {
+            Console.WriteLine("Hello!");
+            Console.WriteLine("Lucille");
+        }
+
+        private static void HW1_quest2()
+        {
+            int a = 5;
+            int b = 6;
+            int c = a + b;
+            Console.WriteLine("Sum = " + (c)); //Sum = 11
+            Console.ReadKey();
+            Console.Write("\n\n");
+
+        }
+
+        private static void HW1_quest3()
+        {
+            Console.WriteLine("First Operation: " + -1 + 4 * 6);
+            Console.ReadLine();
+
+            Console.WriteLine("Second Operation: " + (35 + 5) % 7);
+            Console.ReadLine();
+
+            Console.WriteLine("Third Operation: " + 14 + -4 * 6 / 11);
+            Console.ReadLine();
+
+            Console.WriteLine("Fourth Operation: " + 2 + 15 / 6 * (1 - 7 % 2));
+            Console.ReadLine();
+            Console.Write("\n\n");
+        }
+
+        private static void HW2_quest4()
+        {
+            Console.Write(" please enter First number: ");
+            int num1 = int.Parse(Console.ReadLine());
+
+            Console.Write(" please enter Second number: ");
+            int num2 = int.Parse(Console.ReadLine());
+
+            Console.Write(" please enter Third number: ");
+            int num3 = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Multiplication of 3 numbers = " + num1 * num2 * num3);
+            Console.ReadKey();
+            Console.Write("\n\n");
+        }
+
+        private static void HW2_quest5()
+        {
+            int j, n;
+            Console.Write("Display the multiplication table:\n");
+            Console.Write("-----------------------------------");
+            Console.Write("\n\n");
+
+            Console.Write("Input the number (Table to be calculated) : ");
+            n = Convert.ToInt32(Console.ReadLine());
+            Console.Write("\n");
+            for (j = 1; j <= 10; j++)
+            {
+                Console.Write("{0} X {1} = {2} \n", n, j, n * j);
+            }
+            Console.Write("\n\n");
+        }
+
+        private static void HW2_quest6()
+        {
+            Console.Write(" please enter First number: ");
+            int av1= int.Parse(Console.ReadLine());
+
+            Console.Write(" please enter Second number: ");
+            int av2 = int.Parse(Console.ReadLine());
+
+            Console.Write(" please enter Third number: ");
+            int av3 = int.Parse(Console.ReadLine());
+
+            Console.Write(" please enter Fourth number: ");
+            int av4 = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Average of numbers = " + (av1 + av2 + av3 + av4)/4);
+            Console.ReadKey();
+            Console.Write("\n\n");
+
+        }
+        private static void HW2_quest7()
+        {
+
+        }
+
+    }
+
+}
